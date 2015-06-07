32fda18fb2da1e7098f3b24b7ea2489ae387fb0d
jxv <jxv@hush.com>
2015-06-07 12:56:51 -0700

    Add compare fn; Add .gitignore

--- include/str.h
+++ include/str.h
@@ -6,11 +6,14 @@
 struct str {
 	char *data;
 	size_t len;
-	size_t size;
+	size_t cap;
 };
 
 typedef struct str str_t;
 
 void get_line(str_t *s);
+
+int str_cmp(const str_t *s1, const str_t *s2);
+int str_ncmp(const str_t *s1, const str_t *s2, size_t n);
 
 #endif
--- src/str.c
+++ src/str.c
@@ -3,9 +3,9 @@
 
 void get_line(str_t *s)
 {
-	s->len = s->size;
-	s->data[s->size - 1] = '\0';
-	for (size_t i = 0; i < s->size - 1; i++) {
+	s->len = s->cap;
+	s->data[s->cap - 1] = '\0';
+	for (size_t i = 0; i < s->cap - 1; i++) {
 		const char c = getchar();
 		s->data[i] = c;
 		if (c == '\n') {
@@ -14,4 +14,26 @@ void get_line(str_t *s)
 			break;
 		}
 	}
+}
+
+int str_cmp(const str_t *s1, const str_t *s2)
+{
+	for (size_t i = 0; i < s1->len && i < s2->len; i++) {
+		if (s1->data[i] < s2->data[i])
+			return -1;
+		if (s1->data[i] > s2->data[i])
+			return 1;
+	}
+	return s1->len == s2->len ? 0 : (s1->len < s2->len ? -1 : 1);
+}
+
+int str_ncmp(const str_t *s1, const str_t *s2, size_t n)
+{
+	for (size_t i = 0; i < n; i++) {
+		if (s1->data[i] < s2->data[i])
+			return -1;
+		if (s1->data[i] > s2->data[i])
+			return 1;
+	}
+	return 0;
 }
